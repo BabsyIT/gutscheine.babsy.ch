c3ab7297cae3c2eb966e600da512f8732b8c20a0
stefan-ffr <stefan.mueller.1694@gmail.com>
2025-11-01 18:23:34 +0100

    1

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: false
 };
 
 export default withNextIntl(nextConfig);
